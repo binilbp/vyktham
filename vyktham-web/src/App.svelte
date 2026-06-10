22ca620d16c09104c478e264bc763b16bf53c978
binilbp <biniloffcl@gmail.com>
2026-06-10 16:04:37 +0530

    Better padding; Add Settings icon

--- vyktham-web/src/App.svelte
+++ vyktham-web/src/App.svelte
@@ -5,21 +5,23 @@
     import MainBoard from './lib/MainBoard.svelte'
 
     //icons
-    import Sun from '@lucide/svelte/icons/sun'
+    // import { Sun, Cog } from '@lucide/svelte/icons/sun'
+    import { Sun, Cog } from '@lucide/svelte'
+    // import { Cog } from '@lucide-svelte'
     import Github from './assets/Github.svelte'
 
 
 </script>
 
 <div class="flex flex-col justify-between h-dvh {appSettings.colors.background}">
-    <header class="px-30 py-4 flex flex-row justify-between items-center ">
+    <header class="px-10 py-4 flex flex-row justify-between items-center ">
         <h1 class="font-cascadia text-2xl {appSettings.colors.heading}">
             Vyktham
         </h1>     
 
         <nav class="flex flex-row justify-between items-center gap-6">
-            <h3 class="font-fragment {appSettings.colors.muted}"> <Sun size={19}/> </h3> 
-            <h3 class="font-fragment text-sm {appSettings.colors.muted}"> settings </h3> 
+            <div class="{appSettings.colors.muted}"> <Sun size={19}/> </div> 
+            <div class="{appSettings.colors.muted}"> <Cog size={19}/> </div> 
         </nav>
     </header>
 
@@ -27,7 +29,7 @@
         <MainBoard />
     </main>
 
-    <footer class="px-30 py-4 flex flex-row justify-between ">
+    <footer class="px-10 py-4 flex flex-row justify-between ">
         <section class="flex flex-row justify-between gap-10">
             <h3 class="font-fragment text-sm {appSettings.colors.muted}"> About </h3> 
             
