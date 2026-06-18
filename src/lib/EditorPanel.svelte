5bc05c01431e25a1e5ffdad0584ad4079b73b00b
binilbp <biniloffcl@gmail.com>
2026-06-18 12:51:26 +0530

    Create lib with NotesPanel, EditorPane, FilePanel and import them

---
+++ src/lib/EditorPanel.svelte
@@ -0,0 +1,1 @@
+<h2> this is editor panel </h2>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,15 @@
 <script>
+    import NotesPanel from '$lib/NotesPanel.svelte'
+    import EditorPanel from '$lib/EditorPanel.svelte'
+    import FilePanel from '$lib/FilePanel.svelte'
+
     
 </script>
 
 <div class="h-dvh flex flex-row justify-between">
-    <p>hello tauri</p>
-    <p>hello tauri</p>
-    <p>hello tauri</p>
-
+    <NotesPanel />
+    <EditorPanel />
+    <FilePanel />
 </div>
 
 
